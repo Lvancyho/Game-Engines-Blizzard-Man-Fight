78da58d75549cc8efc545b9cc21745390e9a81b0
Lvancyho <ivanhocy@gmail.com>
2024-12-04 13:07:19 -0500

    added a tiny bit of psuedocode

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
+        //a to move left
+        //d to move right
+        //spacebar to jump
 
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-
-        }
+        //if megaman touches a snowflake
+            //a to move right
+            //d to move left
     }
 }
---
+++ Assets/Scripts/Singleton/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance;
+
+	private void Awake()
+	{
+		if (Instance != null) //creates and sets the instance
+		{
+			Destroy(gameObject);
+			return;
+		}
+		Instance = this;
+	}
+
+	public void CallRestart() //public call for restart
+	{
+		restart();
+	}
+	private void restart() //reloads the scene
+	{
+		SceneManager.LoadScene("SampleScene");
+	}
+}
